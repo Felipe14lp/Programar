bbc3cef8bf4bcfbc8af708267cc08b4ec8cffede
Felipe14lp <luizfelipefelipe003@gmail.com>
2026-04-20 19:28:53 -0300

    Add files via upload

---
+++ atividade do curso de python.py
@@ -0,0 +1,9 @@
+#Atividade de python
+#Crie uma linguagem que leia o dia, mês e o ano de nascimento de uma pessoa e mostra uma mensagem com a data formada
+nome = "Luiz Felipe"
+dia = "14"
+mês = "Novembro"
+anodenascimento = "2006"
+print (nome,dia, mês, anodenascimento)
+print("Olá, seja bem vindo " + nome + ", Parabéns pelo seu aniversário no dia " + dia + " de " + mês + " de " + anodenascimento + ", tenha um ótimo dia!")
+
